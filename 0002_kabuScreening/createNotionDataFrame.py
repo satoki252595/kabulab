a3705d3657df01c3d8ef718c188fbfe10e48201c
satoki252595 <satoki252595@gmail.com>
2024-11-13 08:03:51 +0900

    v0.2

--- 0002_kabuScreening/createNotionDataFrame.py
+++ 0002_kabuScreening/createNotionDataFrame.py
@@ -67,7 +67,7 @@ def createNotionUpDataFrame(codeList: list) -> pd.core.frame.DataFrame:
             'operating_profit_margin': stockInfo['財務情報']['営業利益率'].iloc[0],
             'roe': stockInfo['基本情報']['ROE'],
             'countOperatingProfitMargin': [countOperatingProfitMargin(stockInfo)],
-            'score': [abs(countOperatingProfitMargin(stockInfo) * calAvgRevenueGrowthRate(stockInfo))]
+            'score': [countOperatingProfitMargin(stockInfo) * calAvgRevenueGrowthRate(stockInfo)]
         }
 
         df = pd.DataFrame(data)
@@ -81,7 +81,7 @@ def createNotionUpDataFrame(codeList: list) -> pd.core.frame.DataFrame:
 if __name__ == '__main__':
     # allCodeList = gsfi.getStockCodeDataFrame()
     # codeList = s.screen_stocks(allCodeList)
-    codeList = ['1491.T', '1776.T', '1948.T', '1964.T', '1966.T', '2136.T', '2179.T', '2221.T', '2389.T', '2435.T', '2806.T', '2901.T', '2924.T', '3041.T', '3089.T', '3189.T', '3299.T', '3416.T', '3681.T', '3691.T', '3696.T', '3777.T', '3779.T', '3798.T', '3842.T', '3847.T', '3850.T', '3900.T', '3909.T', '3944.T', '3947.T', '3991.T', '4078.T', '4248.T', '4310.T', '4319.T', '4461.T', '4484.T', '4498.T', '4538.T', '4685.T', '4709.T', '4957.T', '5285.T',
-                '5380.T', '5386.T', '5660.T', '6039.T', '6149.T', '6164.T', '6180.T', '6203.T', '6226.T', '6232.T', '6240.T', '6408.T', '6757.T', '6817.T', '6835.T', '6898.T', '6907.T', '7073.T', '7422.T', '7602.T', '7687.T', '7709.T', '7719.T', '7781.T', '7793.T', '7957.T', '7971.T', '7991.T', '8005.T', '8006.T', '8107.T', '8624.T', '8732.T', '8747.T', '8860.T', '8996.T', '9028.T', '9130.T', '9193.T', '9332.T', '9742.T', '9776.T', '9795.T', '9845.T', '9853.T']
+    codeList = ['6180.T','6358.T' ]
 
-    createNotionUpDataFrame(codeList)
+    df = createNotionUpDataFrame(codeList)
+    print(df)
--- 0002_kabuScreening/notionUp.py
+++ 0002_kabuScreening/notionUp.py
@@ -36,13 +36,18 @@ def uploadToNotion(df):
     }
 
     today = datetime.today().strftime('%Y-%m-%d')
+    
+    # market_capを億円単位に変換し、文字列としてフォーマットするlambda関数
+    format_market_cap = lambda value: f"{value / 100000000:.2f}億円"
+    # roeを小数点第2位で四捨五入するlambda関数
+    format_roe = lambda value: f"{round(value, 2)}"
 
     # スコア順にソート
     df_sorted = df.sort_values(by='score', ascending=False)
 
     children = []
     for index, row in df_sorted.iterrows():
-        #100個以上のテーブル情報はnotionにアップロードできないため、100個までに制限
+        #100個以上のテーブル情報はnotionにアップロードできないため、100個までに制限。（ヘッダー行を考慮）
         if index < 99:
             children.append({
                 "object": "block",
@@ -54,11 +59,11 @@ def uploadToNotion(df):
                             "content": str(row["name"]),
                             "link": {"url": f"https://www.buffett-code.com/company/{row['code']}"}
                         }}],
-                        [{"type": "text", "text": {"content": f"{row['market_cap']:,}"}}],
+                        [{"type": "text", "text": {"content": f"{format_market_cap(row['market_cap'])}"}}],
                         [{"type": "text", "text": {"content": str(
                             row["operating_profit_margin"])}}],
                         [{"type": "text", "text": {
-                            "content": str(row["roe"]*100)}}],
+                            "content": str(format_roe(row["roe"]*100))}}],
                         [{"type": "text", "text": {"content": str(
                             row["countOperatingProfitMargin"])+"年"}}],
 
@@ -141,5 +146,4 @@ if __name__ == "__main__":
     import createNotionDataFrame as cndf
 
     # 関数の実行
-    uploadToNotion(cndf.createNotionUpDataFrame(
-        ['1439.T', '1491.T', '1723.T', '1776.T', '1948.T', '1966.T', '2112.T', '2130.T', '2136.T', '2179.T', '2185.T', '2349.T', '2389.T', '2435.T', '2788.T', '2806.T', '2924.T', '3069.T', '3089.T', '3299.T', '3416.T', '3461.T', '3633.T', '3681.T', '3688.T', '3691.T', '3696.T', '3747.T', '3777.T', '3798.T', '3837.T', '3842.T', '3847.T', '3848.T', '3850.T', '3900.T', '3909.T', '4078.T', '4310.T', '4319.T', '4390.T', '4475.T', '4484.T', '4538.T', '5036.T', '5121.T', '5285.T', '5386.T', '5660.T', '5697.T', '5950.T', '6039.T', '6137.T', '6145.T', '6149.T', '6164.T', '6180.T', '6203.T', '6226.T', '6231.T', '6232.T', '6240.T', '6358.T', '6378.T', '6402.T', '6408.T', '6757.T', '6824.T', '6835.T', '6898.T', '6907.T', '6927.T', '7018.T', '7081.T', '7268.T', '7317.T', '7422.T', '7515.T', '7602.T', '7687.T', '7709.T', '7719.T', '7781.T', '7914.T', '7938.T', '7971.T', '7991.T', '8005.T', '8107.T', '8704.T', '8732.T', '8747.T', '8789.T', '8836.T', '8996.T', '9028.T', '9073.T', '9130.T', '9193.T', '9305.T', '9308.T', '9441.T', '9742.T', '9776.T', '9816.T', '9845.T', '9853.T']))
+    uploadToNotion(cndf.createNotionUpDataFrame(['3933.T']))
